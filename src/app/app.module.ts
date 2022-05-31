ee5f69ebd7a3d8ce4ffcfc2e92b4e0f7a91c998c
darwin1998 <cabarrubiasdarwin98@gmail.com>
2022-05-31 09:29:56 +0800

    native camera feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [Camera, { provide: RouteReuseStrategy,  useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy,  useClass: IonicRouteStrategy },Camera],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
